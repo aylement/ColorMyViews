57f47adc7b8f29e407f06b2206c6f6420b225e0f
kaldwar <jalkaru@laposte.net>
2019-11-15 10:41:53 +0100

    last commit

--- app/src/main/java/com/example/android/colormyviews/MainActivity.kt
+++ app/src/main/java/com/example/android/colormyviews/MainActivity.kt
@@ -16,7 +16,9 @@ class MainActivity : AppCompatActivity() {
 
     private fun setListeners() {
         val clickableViews: List<View> =
-            listOf(box_one_text, box_two_text, box_three_text, box_four_text, box_five_text, constraint_layout)
+            listOf(box_one_text, box_two_text, box_three_text,
+                box_four_text, box_five_text, constraint_layout,
+                btn_red, btn_yellow, btn_green)
 
         for(item in clickableViews) {
             item.setOnClickListener{ makeColored(it)}
@@ -32,6 +34,10 @@ class MainActivity : AppCompatActivity() {
             R.id.box_three_text -> view.setBackgroundResource(android.R.color.holo_green_light)
             R.id.box_four_text -> view.setBackgroundResource(android.R.color.holo_green_dark)
             R.id.box_five_text -> view.setBackgroundResource(android.R.color.holo_green_light)
+
+            R.id.btn_red -> box_three_text.setBackgroundResource(R.color.my_red)
+            R.id.btn_yellow -> box_four_text.setBackgroundResource(R.color.my_yellow)
+            R.id.btn_green -> box_five_text.setBackgroundResource(R.color.my_green)
 
             else -> view.setBackgroundColor(Color.LTGRAY)
         }
